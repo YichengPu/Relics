95752a5ab62dae68bb261714709c66b260957cbb
Yicheng <yp653@nyu.edu>
2019-04-21 21:38:13 -0400

    update kmeans to kmeans++

--- models/kmeans.py
+++ models/kmeans.py
@@ -2,8 +2,26 @@ from collections import defaultdict
 import numpy as np
 
 def kmeans(n_iter,K,train_x):
-    start_centroids_index=np.random.choice(np.arange(len(train_x)),K)
-    start_centroids=train_x[start_centroids_index]
+    start_centroids_index=np.random.choice(np.arange(len(train_x)),K,replace=False)
+
+    first_centroid=np.random.choice(np.arange(len(train_x)),1,replace=False)
+    start_centroids=[train_x[first_centroid,:]]
+
+    while len(start_centroids)<K:
+        D_weights=[]
+        for i in range(len(train_x)):
+            min_dist=np.inf
+            for j in range(len(start_centroids)):
+                curr_dist=np.linalg.norm(train_x[i,:]-start_centroids[j])
+                min_dist=min(curr_dist,min_dist)
+            D_weights.append(min_dist**2)
+        D_weights=np.array(D_weights)/sum(D_weights)
+
+        next_centroid=np.random.choice(np.arange(len(train_x)),1,replace=False,p=D_weights)
+        start_centroids.append(train_x[next_centroid,:])
+
+    start_centroids=np.array(start_centroids)
+    # start_centroids=train_x[start_centroids_index]
     centroids=start_centroids
 
     for j in range(n_iter):
